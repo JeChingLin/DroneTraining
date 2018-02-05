a3b73707ee533e1ba853566fe33a3244930d6d7d
Kevin Lin <kevin.lin@orbweb.com>
2018-02-05 16:53:19 +0800

    drone start

---
+++ go-example/main.go
@@ -0,0 +1,13 @@
+packet main
+
+import {
+	"log"
+}
+
+func main() {
+	log.Println("main test")
+}
+
+func ddd() {
+	log.Printf("test func!")
+}
---
+++ go-example/main_test.go
@@ -0,0 +1,16 @@
+
+package main
+
+import (
+	"testing"
+)
+
+func Test_main(t *testing.T) {
+
+	main()
+}
+
+func Test_ddd(t *testing.T){
+	ddd()
+}
+
